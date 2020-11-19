ec81840b81d730f582e8a754ef0d7a6afd93532c
VietVH9 <vietvh9@fsoft.com.vn>
2020-11-19 09:43:46 +0700

    update directory constructor

--- akaocr/utils/data/dataloader.py
+++ akaocr/utils/data/dataloader.py
@@ -1,8 +1,39 @@
+
 import os
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+from utils.file_utils import LmdbReader
+from torch.utils.data import Dataset, ConcatDataset, Subset
+
+class LmdbDataset(Dataset):
+    """
+    Base loader for lmdb type dataset
+    """
+
+    def __init__(self, root, rgb=False, labelproc=None):
+        """
+        :param root: path to lmdb dataset
+        :param label_handler: type of label processing
+        """
+        # self.env, self.num_samples = read_lmdb(root)
+
+        if labelproc is None:
+            warn("You don\'t have label handler")
+
+        self.labelproc = labelproc
+        self.lmdbreader = LmdbReader(root, rgb)
+
+    def __len__(self):
+        return self.lmdbreader.num_samples
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'
+        image, label = self.lmdbreader.lmdb_loader(index)
+        if self.labelproc is not None:
+            label = self.labelproc.process_label(label)
+        return image, label
 
 def get_seg(path):
     f = h5py.File(path, 'r')
