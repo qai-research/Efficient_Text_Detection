c8c3fab78213b3248ab9eb83b1ef7e15b2b82001
VietVH9 <vhv.19032011@gmail.com>
2021-02-17 16:38:50 +0700

    update utils

--- akaocr/synthtext/utils/utils_func.py
+++ akaocr/synthtext/utils/utils_func.py
@@ -116,7 +116,34 @@ def Augmentator(images, points=None, option=None):
     return [255 - np.array(img) for img in images_aug], points
 
 
-
+def check_valid(dataframe, bg_df, source_df, fonts_df):
+    """
+    Check if the valid of input dataframe
+    """
+    df = dataframe.copy()
+    results = {}
+    for index, value in enumerate(dataframe.values):
+        results[index] = {"Status": "valid", "Error": []}
+        Method, NumCores, Fonts, Backgrounds, ObjectSources, Textsources, ImageSources = value[:7]
+        if Backgrounds not in bg_df['NAME'].values:
+            results[index]['Error'].append('Invalid Backgrounds Folder')
+        else:
+            info = bg_df[bg_df['NAME'] == Backgrounds]
+            if Method == 'white' and 'white' not in info['METHOD'].values:
+                results[index]['Error'].append('Invalid Method')
+            if Fonts not in fonts_df['NAME'].values:
+                results[index]['Error'].append('Fonts Folder Is Not Existed.')
+            if str(Textsources) != '0' and Textsources not in source_df['NAME'].values:
+                results[index]['Error'].append('The TextSources Is Not Existed.')
+            if str(ObjectSources) != '0' and ObjectSources not in source_df['NAME'].values:
+                results[index]['Error'].append('The ObjectSources Is Not Existed.')
+            if str(ImageSources) != '0' and ImageSources not in source_df['NAME'].values:
+                results[index]['Error'].append('The ObjectSources Is Not Existed.')
+        if len(results[index]['Error']) is not 0:
+            results[index]["Status"] = "INVALID"
+    df['STATUS'] = [results[i]["Status"] for i in range(len(results))]
+    df['DETAIL'] = [results[i]["Error"] for i in range(len(results))]
+    return df
 
 
 def get_all_valid(background_folder, source_folder, font_folder):
