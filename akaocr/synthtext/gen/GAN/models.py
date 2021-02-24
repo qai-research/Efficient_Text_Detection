c35f9bde346364b6e09daf7574630dad53f46b6e
VietVH9 <vhv.19032011@gmail.com>
2021-02-24 08:19:10 +0700

    update GAN directory

---
+++ akaocr/synthtext/gen/GAN/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+_____________________________________________________________________________
+Created By  : Vu Hoang Viet - VietVH9
+Created Date: NOV - 2020
+Project : AkaOCR core
+_____________________________________________________________________________
+
+
+
+The Module Has Been Build for...
+_____________________________________________________________________________
+"""
+
+import os
+import torch
+import lmdb
+
+class Extraction:
+
+    def __init__(self):
+        pass
+
+    def model(self):
+        pass
+
