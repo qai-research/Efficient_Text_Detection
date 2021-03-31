c2cf33613fcc17724ead99df4f8f0fb60ffdb92b
BacNV6 <huukim98@gmail.com>
2021-03-31 23:11:46 +0700

    Trigger merge-Kimnh3-Update config to get from flag argument; update some default config value

--- akaocr/engine/config.py
+++ akaocr/engine/config.py
@@ -45,6 +45,7 @@ def parse_base(add_help=True):
     parser.add_argument("-w", "--weight", type=str, default=None)
     parser.add_argument("-g", "--gpu", nargs="+")
     parser.add_argument("--data", type=str, default="../data")
+    # return parser.parse_args()
     return parser
 
 
@@ -56,6 +57,7 @@ def setup(tp="recog", args=None):
         exp_path = data_path.joinpath("exp_recog", args.exp)
         exp_config_path = str(exp_path.joinpath(args.exp + "_recog_config.yaml"))
         if not exp_path.exists():
+            config = args.config
             logger.warning(f"Experiment {args.exp} do not exist.")
             logger.warning("Creating new experiment folder")
             exp_exist = False
@@ -70,6 +72,7 @@ def setup(tp="recog", args=None):
         exp_path = data_path.joinpath("exp_detec", args.exp)
         exp_config_path = str(exp_path.joinpath(args.exp + "_detec_config.yaml"))
         if not exp_path.exists():
+            config = args.config
             logger.info(f"Experiment {args.exp} do not exist")
             logger.info("Creating new experiment folder")
             exp_exist = False
