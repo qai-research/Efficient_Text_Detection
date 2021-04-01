1802afed517f6ac1480898d35c2af1cf42464764
BacNV6 <huukim98@gmail.com>
2021-04-01 18:12:19 +0700

    Kimnh3-Add 2 file train (official, not test) seperately; CI yml: add train stage, extract data arguments from commit message

--- akaocr/tools/train_detec.py
+++ akaocr/tools/train_detec.py
@@ -1,27 +1,57 @@
 # -*- coding: utf-8 -*-
 """
 _____________________________________________________________________________
-Created By  : Nguyen Viet Bac - Bacnv6
-Created Date: Fri November 25 16:34:00 VNT 2020
+Created By  : Nguyen Huu Kim - Kimnh3
+Created Date: Mar 31, 2021 6:31pm GMT+0700
 Project : AkaOCR core
 _____________________________________________________________________________
 
-This file contain sample training function
+This file contain code for train detec
 _____________________________________________________________________________
 """
-
 import sys
-sys.path.append("../")
-
-import os
 import torch
 
-from engine.config import setup
+sys.path.append("../")
+from models.detec.heatmap import HEAT
+# from models.detec.resnet_fpn_heatmap import RESNET_FPN_HEAT
+from engine import Trainer
+from engine.config import setup, parse_base
+from engine.trainer.loop import CustomLoopHeat, CustomLoopAtten
+from engine.build import build_dataloader
+from utils.data.dataloader import LoadTestDetecDataset
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+from engine.metric.accuracy import RecogAccuracy
+from engine.metric.evaluation import RecogEvaluation
+
+def test_detec(args):
+    cfg = setup("detec", args)
+    cfg.MODEL.NUM_CLASS = 3210
+    cfg.SOLVER.DEVICE = str(device)
+    cfg.SOLVER.DATA_SOURCE = args.data_detec
+
+    model = HEAT(cfg)
+    # model = RESNET_FPN_HEAT(cfg)
+    model.to(device=device)
+
+    evaluate = DetecEvaluation(cfg)
+    acc = DetecAccuracy(cfg)
+    lossc = CustomLoopHeat(cfg)
+    train_loader = build_dataloader(cfg, args.data_detec)
+    test_loader = LoadTestDetecDataset(args.data_test_detec, cfg)
+    trainer = Trainer(cfg, model, train_loader=train_loader, test_loader=test_loader, custom_loop=lossc, accuracy=acc,
+                      evaluation=evaluate, resume=True)
+    trainer.do_train()
 
 
 def main():
-    args = setup("detec")
-
+    parser = parse_base()
+    parser.add_argument('--data_detec', type=str, help='path to detect data')
+    parser.add_argument('--data_test_detec', type=str, help='path to test detect data')
+    args = parser.parse_args()
+    test_detec(args)
 
 if __name__ == '__main__':
     main()
