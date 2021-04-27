56a834e7410c9ffc9959eb3f57ad1f957509539f
huukim911 <huukim911@gmail.com>
2021-04-27 16:59:06 +0700

    Add script sh to public code

--- public.sh
+++ public.sh
@@ -9,4 +9,4 @@ git reset HEAD akaocr/models/detec/resnet_fpn_heatmap.py akaocr/models/modules/b
 git status
 git commit -m "Update code"
 git push origin qaicom
-git push public qaicom
+git push -f public qaicom
