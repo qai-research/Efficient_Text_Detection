00366c263a08f1bf42aa4decb9d67fb85a127a31
huukim911 <huukim911@gmail.com>
2021-04-27 17:08:49 +0700

    Add script sh to public code

--- public.sh
+++ public.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
+# """
+# _____________________________________________________________________________
+# Created By  : Nguyen Huu Kim - Kimnh3
+# Created Date: Apr 27, 2021 10:00am GMT+0700
+# Project : AkaOCR core
+# _____________________________________________________________________________
+
+# This file contain sh script for update feature for public repo
+# _____________________________________________________________________________
+# """
 git remote -v
 git remote add public https://github.com/qai-research/Efficient_Text_Detection.git
-cd ocr-components
 git checkout qaicom
 git status
 git checkout master -- akaocr/
