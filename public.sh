096716263c122079dc977568156fc626c25e1c45
huukim911 <huukim911@gmail.com>
2021-04-27 16:58:11 +0700

    Add script sh to public code

---
+++ public.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git remote -v
+git remote add public https://github.com/qai-research/Efficient_Text_Detection.git
+cd ocr-components
+git checkout qaicom
+git status
+git checkout master -- akaocr/
+git reset HEAD akaocr/models/detec/resnet_fpn_heatmap.py akaocr/models/modules/backbones/Resnet_Extractor.py akaocr/synthtext/* akaocr/data/*
+git status
+git commit -m "Update code"
+git push origin qaicom
+git push public qaicom
