caebfd437e30451da23de152e27ba14d8785f53e
huukim911 <huukim98@gmail.com>
2021-05-10 13:41:34 +0700

    Add lmdb maker, code convert for icdar 13 dataset

--- akaocr/dataprep/prepare.sh
+++ akaocr/dataprep/prepare.sh
@@ -18,21 +18,27 @@ wget -O COCO_Text.zip https://s3.amazonaws.com/cocotext/COCO_Text.zip
 # unzip
 unzip train2014.zip
 unzip COCO_Text.zip
-# run convert dataset for cocotext (ex. on 72)
-python code/convert_cocotext.py --src_path train2014 --annot_path COCO_Text.json --des_path cocotext_gt/train
-# create lmdb from output gt (cocotext/train)
+# run convert dataset(ex. on 72)
+python code/convert_cocotext.py --src_path train2014 --annot_path COCO_Text.json --des_path cocotext
 
-# SCUT-CTW1500 dataset
-# train
-wget -O train_images.zip https://universityofadelaide.box.com/shared/static/py5uwlfyyytbb2pxzq9czvu6fuqbjdh8.zip
-wget -O train_labels.zip https://universityofadelaide.box.com/shared/static/jikuazluzyj4lq6umzei7m2ppmt3afyw.zip
+# ICDAR15
+wget -O ch4_training_images.zip https://rrc.cvc.uab.es/downloads/ch4_training_images.zip
+wget -O ch4_training_localization_transcription_gt.zip https://rrc.cvc.uab.es/downloads/ch4_training_localization_transcription_gt.zip
 
-#test
-wget -O test_images.zip https://universityofadelaide.box.com/shared/static/t4w48ofnqkdw7jyc4t11nsukoeqk9c3d.zip
-wget -O test_labels.zip https://cloudstor.aarnet.edu.au/plus/s/uoeFl0pCN9BOCN5/download
+wget -O ch4_test_images.zip https://rrc.cvc.uab.es/downloads/ch4_test_images.zip
+wget -O Challenge4_Test_Task1_GT.zip https://rrc.cvc.uab.es/downloads/Challenge4_Test_Task1_GT.zip
 
 # unzip
-unzip train_images.zip
-unzip train_labels.zip
-# for ctw1500
-python convert_ctw1500.py --img_path train_images --ann_path ctw1500_train_labels --des_path ctw1500/train
+unzip ch4_training_images.zip -d ch4_training_images
+unzip ch4_training_localization_transcription_gt.zip -d ch4_training_localization_transcription_gt
+unzip ch4_test_images.zip -d ch4_test_images
+unzip Challenge4_Test_Task1_GT.zip -d Challenge4_Test_Task1_GT
+
+# run convert dataset (ex. on 72)
+python code/convert_icdar13.py --train_img ch4_training_images --train_gt ch4_training_localization_transcription_gt --test_img ch4_test_images --test_gt Challenge4_Test_Task1_GT --des_path icdar13
+# python code/convert_icdar13.py --train_img D:\kimnh3\dataset\ocr\raw_download\ICDAR13\1.localization\Challenge2_Training_Task12_Images --train_gt D:\kimnh3\dataset\ocr\raw_download\ICDAR13\1.localization\Challenge2_Training_Task1_GT --test_img D:\kimnh3\dataset\ocr\raw_download\ICDAR13\1.localization\Challenge2_Test_Task12_Images --test_gt D:\kimnh3\dataset\ocr\raw_download\ICDAR13\1.localization\Challenge2_Test_Task1_GT --des_path D:\kimnh3\dataset\ocr\converted\icdar13
+
+#convert lmdb
+
+# ICDAR15
+python code/convert_icdar15.py --train_img D:\kimnh3\dataset\ocr\raw_download\ICDAR15\ch4_training_images --train_gt D:\kimnh3\dataset\ocr\raw_download\ICDAR15\ch4_training_localization_transcription_gt --test_img D:\kimnh3\dataset\ocr\raw_download\ICDAR15\ch4_test_images --test_gt D:\kimnh3\dataset\ocr\raw_download\ICDAR15\Challenge4_Test_Task1_GT --des_path D:\kimnh3\dataset\ocr\converted\icdar15
