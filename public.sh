7a42e65438ba5bdbc3355957b231182a7d0ece91
huukim911 <huukim98@gmail.com>
2021-05-12 17:28:09 +0700

    Update

--- public.sh
+++ public.sh
@@ -17,6 +17,6 @@ git status
 git checkout master -- akaocr/
 git reset HEAD akaocr/models/detec/resnet_fpn_heatmap.py akaocr/models/modules/backbones/Resnet_Extractor.py akaocr/synthtext/* akaocr/data/*
 git status
-git commit -m "Update code"
+git commit -m "Update features"
 git push origin qaicom
 git push -f public qaicom
