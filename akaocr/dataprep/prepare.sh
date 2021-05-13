e82b0c837c6747ff7441624c7b379da514c4e875
huukim911 <huukim98@gmail.com>
2021-05-14 01:05:03 +0700

    Re-design lmdb structure to match train scripts

--- akaocr/dataprep/prepare.sh
+++ akaocr/dataprep/prepare.sh
@@ -24,9 +24,10 @@ unzip -n COCO_Text.zip
 # run convert dataset(ex. on 72)
 python code/convert_cocotext.py --src_path train2014 --annot_path COCO_Text.json --des_path cocotext || export failed=1
 #convert lmdb
-mkdir -p ../data/cocotext
+mkdir -p ../data/cocotext ../data/cocotext/train ../data/cocotext/test
 python code/seq_detectolmdb.py --torecog 0 --input cocotext --output ../data/cocotext || export failed=1
-
+mv ../data/cocotext/ST/ST_train/ ../data/cocotext/train/.
+mv ../data/cocotext/ST/ST_test/ ../data/cocotext/test/.
 # ICDAR13
 wget -nc -O Challenge2_Training_Task12_Images.zip https://rrc.cvc.uab.es/downloads/Challenge2_Training_Task12_Images.zip
 wget -nc -O Challenge2_Training_Task1_GT.zip https://rrc.cvc.uab.es/downloads/Challenge2_Training_Task1_GT.zip
@@ -45,9 +46,10 @@ unzip -n Challenge2_Test_Task1_GT.zip -d Challenge2_Test_Task1_GT
 # run convert dataset (ex. on 72)
 python code/convert_icdar13_char.py --train_img Challenge2_Training_Task12_Images --train_gt Challenge2_Training_Task1_GT --train_ch_gt Challenge2_Training_Task2_GT --test_img Challenge2_Test_Task12_Images --test_gt Challenge2_Test_Task1_GT --des_path icdar13 || export failed=1
 #convert lmdb
-mkdir -p ../data/icdar13
+mkdir -p ../data/icdar13 ../data/icdar13/train ../data/icdar13/test
 python code/seq_detectolmdb.py --torecog 0 --input icdar13 --output ../data/icdar13 || export failed=1
-
+mv ../data/icdar13/ST/ST_train/ ../data/icdar13/train/.
+mv ../data/icdar13/ST/ST_test/ ../data/icdar13/test/.
 # ICDAR15
 wget -nc -O ch4_training_images.zip https://rrc.cvc.uab.es/downloads/ch4_training_images.zip
 wget -nc -O ch4_training_localization_transcription_gt.zip https://rrc.cvc.uab.es/downloads/ch4_training_localization_transcription_gt.zip
@@ -64,9 +66,10 @@ unzip -n Challenge4_Test_Task1_GT.zip -d Challenge4_Test_Task1_GT
 # run convert dataset (ex. on 72)
 python code/convert_icdar15.py --train_img ch4_training_images --train_gt ch4_training_localization_transcription_gt --test_img ch4_test_images --test_gt Challenge4_Test_Task1_GT --des_path icdar15 || export failed=1
 #convert lmdb
-mkdir -p  ../data/icdar15
+mkdir -p  ../data/icdar15  ../data/icdar15/train ../data/icdar15/test
 python code/seq_detectolmdb.py --torecog 0 --input icdar15 --output ../data/icdar15 || export failed=1
-
+mv ../data/icdar15/ST/ST_train/ ../data/icdar15/train/.
+mv ../data/icdar15/ST/ST_test/ ../data/icdar15/test/.
 
 # Synthtext800k
 # download
@@ -85,12 +88,16 @@ mv synthtext800k/synthtext_p9 synthtext_test/.
 mv synthtext800k/synthtext_p* synthtext_train/.
 
 # create for 1 test sets (ST_synthtext_p9)
-mkdir -p ../../data/synthtext/synthtext_test
-python ../code/seq_detectolmdb.py --torecog 0 --input synthtext_test --output ../../data/synthtext/synthtext_test || export failed=1
+mkdir -p ../../data/synthtext/test
+python ../code/seq_detectolmdb.py --torecog 0 --input synthtext_test --output ../../data/synthtext/test || export failed=1
+mv ../data/synthtext/test/ST/ST_synthtext_p9/ ../data/synthtext/test/.
+rm -rf ../data/synthtext/test/ST
 
 # create for 8 train sets (ST_synthtex_p1...8)
-mkdir -p ../../data/synthtext/synthtext_train
-python ../code/seq_detectolmdb.py --torecog 0 --input synthtext_train --output ../../data/synthtext/synthtext_train || export failed=1
+mkdir -p ../../data/synthtext/train
+python ../code/seq_detectolmdb.py --torecog 0 --input synthtext_train --output ../../data/synthtext/train || export failed=1
+mv ../data/synthtext/train/ST/ST_synthtext_p*/ ../data/synthtext/train/.
+rm -rf ../data/synthtext/train/ST
 
 cd ../../
 
